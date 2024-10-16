970eabec0dbe74d41e9e83ac77417ab43cb87b82
이지윤 <02leejiyun@naver.com>
2024-10-16 21:12:14 +0900

    Initial commit

---
+++ app/src/main/java/com/olbareum/olbareum/RecordActivity.kt
@@ -0,0 +1,19 @@
+package com.olbareum.olbareum
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.olbareum.olbareum.databinding.ActivityRecordBinding
+
+class RecordActivity : AppCompatActivity() {
+
+}
