af263dcde6bc6ccf69c4286dffff8fdb6d70c916
이지윤 <02leejiyun@naver.com>
2024-10-30 18:59:10 +0900

    feat: 로그인 UI 구현

---
+++ app/src/main/java/com/olbareum/olbareum/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.olbareum.olbareum
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.olbareum.olbareum.databinding.ActivityLoginBinding
+import com.olbareum.olbareum.record.RecordActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, RecordActivity::class.java))
+        }
+    }
+}
