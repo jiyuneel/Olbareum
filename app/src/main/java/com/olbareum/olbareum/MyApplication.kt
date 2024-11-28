bfea9decfd68eea52eb45fed3f626b20b76e6397
이지윤 <02leejiyun@naver.com>
2024-11-28 17:21:13 +0900

    feat: 다크모드 비활성화

--- app/src/main/java/com/olbareum/olbareum/MyApplication.kt
+++ app/src/main/java/com/olbareum/olbareum/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.olbareum.olbareum
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class MyApplication : Application() {
     companion object {
@@ -10,5 +11,8 @@ class MyApplication : Application() {
     override fun onCreate() {
         preferences = PreferenceUtil(applicationContext)
         super.onCreate()
+
+        // 다크모드 비활성화
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
